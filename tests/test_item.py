2c0cbb795186cf56b8f2e4e8003f733a53573da8
zulfat <89528074642@mail.ru>
2023-09-05 22:39:01 +0700

    initial commit 1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+Item.pay_rate = 0.75
+@pytest.fixture
+def smartphone_info():
+    return Item("smartphone", 25000, 8)
+
+
+def test_calculate_total_price(smartphone_info):
+    assert smartphone_info.calculate_total_price() == 200000.0
+
+
+def test_apply_discount(smartphone_info):
+    assert smartphone_info.apply_discount() == None
